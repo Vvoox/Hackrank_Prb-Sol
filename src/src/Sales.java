92db763a57ad90b057174c4c87d050887efab3a1
Vvoox <khalil.daoulat@gmail.com>
2020-02-05 03:06:34 +0100

    Sales solved (last sol)

--- src/src/Sales.java
+++ src/src/Sales.java
@@ -13,87 +13,41 @@ import static java.util.stream.Collectors.toList;
 
 
 class Result {
-
-    /*
-     * Complete the 'balancedSum' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY sales as parameter.
-     */
-
     public static int balancedSum(List<Integer> sales) {
-
         int n = sales.size();
-//        int leftSum = sales.get(0);
-//        int rightSum = sales.get(n-1);
-        int L = n - 2;
-        int R = n - 1;
-        int balancedN = 1;
-        int result = 0;
-
-        while(balancedN<n-1){
+        int result=0;
+        sales.subList(0,1);
+        for(int balancedN =1;balancedN<n-1;balancedN++){
 
             if(leftSum(sales,balancedN)==rightSum(sales,balancedN)){
 
                 result = balancedN;
+                break;
 
             }
-
-
-            balancedN++;
         }
         System.out.println(result);
         return result;
     }
 
     public static int leftSum(List<Integer> leftSales,int index){
-        int i=0;
         int sum= 0;
-        while(i<index){
+        for(int i:leftSales.subList(0,index)){
 
-            sum+=leftSales.get(i);
-            i++;
+            sum+=i;
         }
-        System.out.println("left sum "+sum);
+
         return sum;
     }
-    public static int rightSum(List<Integer> leftSales,int index){
-        int i= leftSales.size()-1;
+    public static int rightSum(List<Integer> rightSales,int index){
         int sum= 0;
-        while(i>index){
-
-            sum+=leftSales.get(i);
-            i--;
+        int n = rightSales.size();
+        for(int i :rightSales.subList(index+1,n)){
+            sum+=i;
         }
-        System.out.println("right sum :"+sum);
         return sum;
     }
-//
-//
-//        for (int index = 1; index < n - 1; index++) {
-//
-//            int leftsum = 0;
-//            int rightsum = 0;
-//            for (int left = 0; left < index; left++) {
-//                leftsum += sales.get(left);
-//            }
-//            for (int right = index + 1; right < n; right++) {
-//
-//                rightsum += sales.get(right);
-//            }
-//            System.out.println("left is : " + leftsum);
-//            System.out.println("right is : " + rightsum);
-//
-//            if(leftsum==rightsum){
-//
-//                result=index;
-//            }
-//
-//
-//
-//        }
-//        System.out.println(result);
-//
+
 
 }
 
@@ -101,13 +55,15 @@ class Result {
 public class Solution {
     public static void main(String[] args) throws IOException {
 
-
+        long startTime = System.nanoTime();
         List<Integer> sales = new ArrayList<>();
         sales.add(1);
         sales.add(2);
         sales.add(1);
         sales.add(3);
         Result.balancedSum(sales);
-
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+        System.out.println(duration/10000);
     }
 }
