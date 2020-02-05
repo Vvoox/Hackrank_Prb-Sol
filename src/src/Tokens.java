1f81d86a4550c6a00645492aaa1a0a9779f11124
Vvoox <khalil.daoulat@gmail.com>
2020-02-05 06:49:26 +0100

    Still

--- src/src/Tokens.java
+++ src/src/Tokens.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import org.omg.PortableServer.LIFESPAN_POLICY_ID;
+import sun.java2d.opengl.GLXSurfaceData;
+
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
@@ -17,12 +21,16 @@ import java.util.stream.*;
 
 import static java.util.stream.Collectors.*;
 
-public class Tokens {
+public class Solution {
 
     public static void main(String[] args) throws IOException {
+        long startTime = System.nanoTime();
+
         List<List<Integer>> commands = new ArrayList<>();
 
         int expiryLimit = 3;
+        int tok;
+        int n = commands.size();
 
         List<Integer> a1 = new ArrayList<>();
         a1.add(0);
@@ -34,30 +42,55 @@ public class Tokens {
         a2.add(4);
         List<Integer> a3 = new ArrayList<>();
         a3.add(1);
-        a3.add(6);
+        a3.add(2);
         a3.add(5);
+        List<Integer> a4 = new ArrayList<>();
+        a4.add(0);
+        a4.add(3);
+        a4.add(7);
+        List<Integer> a5 = new ArrayList<>();
+        a5.add(1);
+        a5.add(3);
+        a5.add(8);
+
 
         commands.add(a1);
         commands.add(a2);
         commands.add(a3);
+        commands.add(a4);
+        commands.add(a5);
+
+
 
         System.out.println(expiryLimit);
         System.out.println(commands.size());
         System.out.println(commands.get(0).size());
 
-        for(int i =0 ; i<commands.size() ; i++){
-            for(int j = 0 ; j<commands.get(0).size(); j++){
+        for (int i = 0; i < commands.size(); i++) {
+            for (int j = 0; j < commands.get(0).size(); j++) {
 
-                System.out.print(commands.get(i).get(j)+" ");
+                System.out.print(commands.get(i).get(j) + " ");
             }
             System.out.println();
 
         }
-//        Result.numberOfTokens(expiryLimit, commands);
+        int status = 0;
 
-//        int result = Result.numberOfTokens(expiryLimit, commands);
-        Result.test(expiryLimit, commands);
+        for(int i=0 ; i < commands.size() ; i++){
+            System.out.println(Result.tokenRunning(i, expiryLimit, commands));
+            if(Result.tokenRunning(i, expiryLimit, commands)){
+                status+=1;
+            }
 
+        }
+        System.out.println(status);
+
+
+
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Time : " + duration / 10000);
 
     }
 
@@ -65,125 +98,41 @@ public class Tokens {
 
 class Result {
 
-    public static int numberOfTokens(int expiryLimit, List<List<Integer>> commands) {
+    public static boolean tokenRunning(int token , int expiryLimit , List<List<Integer>> commands){
 
-        int T = 0;
-        int time_running = 0;
-        int token_running = 0;
-//        List<List<Integer>> token = new ArrayList<>();
-//
-//
-//        int l=0;
-//        for(int j = 0 ; j<commands.size() ; j++){
-//
-//            List<Integer> t = new ArrayList<>();
-//
-//            if (commands.get(l).get(1).equals(commands.get(j).get(1))) {
-//
-//                t.add(commands.get(j).get(0));
-//                t.add(commands.get(j).get(2));
-//
-//            }
-//            token.add(t);
-//
-//            if(j==commands.size()-1){
-//                break;}
-//
-//
-//            }
-//
-//
-//
-//        for(int i = 0 ; i<token.size();i++){
-//
-//            for(int j=0 ; j<token.get(0).size() ; j++){
-//
-//                System.out.print(token.get(i).get(j)+" ");
-//
-//            }
-//            System.out.println();
-//
-//        }
-//
-////        return expiryLimit;
-//    }
-        for (int i = 0; i < commands.size(); i++) {
+        int n = commands.size();
+        int i=0;
+        int start = 0;
+        int finish = 0;
+        boolean status = false;
+        while(i<n){
+            if(commands.get(i).get(1)==token){
 
-            boolean get = false, reset = false;
-            if (commands.get(i).get(0).equals(0)) {
-                get = true;
-                reset = false;
-            }
-            if (commands.get(i).get(0).equals(1)) {
-                get = false;
-                reset = true;
-            }
-
-            if (get) {
-
-                token_running = commands.get(i).get(1);
-                time_running = commands.get(i).get(2) + expiryLimit;
-                System.out.println(time_running);
-            }
-            if (reset) {
-                System.out.println(commands.get(i).get(2));
-
-                if (time_running >= commands.get(i).get(2)) {
-                    time_running = commands.get(i).get(2) + expiryLimit;
+                if(commands.get(i).get(0)==0){
+                    start=commands.get(i).get(2);
+                    finish =commands.get(i).get(2)+expiryLimit ;
                 }
-                else{
-                    token_running-=1;
+                if(commands.get(i).get(0)==1){
+
+                    if(commands.get(i).get(2)>finish){
+                        status = false;
+                        break;
+                    }
+                    else{
+                        start = commands.get(i).get(2);
+                        finish =commands.get(i).get(2)+expiryLimit ;
+                        status=true;
+                    }
+
                 }
-            }
-
-
-
-        }
-        return token_running;
-
-
-
-
-    }
-    public static void test(int expiryLimit, List<List<Integer>> commands) {
-        int max = commands.get(0).get(1);
-        for(int i = 0 ; i< commands.size() ; i++){
-
-            if(max <= commands.get(i).get(1)){
-
-                max = commands.get(i).get(1);
-            }
-
-        }
-        System.out.println("max of tokens id  : "+max);
-
-        int tokens_number = 1;
-        int t = commands.get(0).get(1);
-        for (int i = 0; i < commands.size(); i++) {
-
-            if (t != commands.get(i).get(1)) {
-
-                t = commands.get(i).get(1);
-                tokens_number += 1;
-            }
-        }
-        System.out.println(tokens_number);
-
-        int n = commands.get(0).get(1);
-        while (n <= max) {
-            int status = 0;
-            int i=0;
-            int token_id=commands.get(i).get(1);
-            if (token_id == commands.get(n).get(1)) {
-                status += 1;
 
             }
-
-            System.out.println("token " + token_id + " times : " + status);
-            n++;
             i++;
 
         }
+
+        return status;
+
     }
 
 
