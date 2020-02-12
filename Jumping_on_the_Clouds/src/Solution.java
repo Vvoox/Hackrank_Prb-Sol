a0e4daa5ecc8fb6b385d69a8d65d1020aa9f6d7d
Vvoox <khalil.daoulat@gmail.com>
2020-02-12 23:23:34 +0100

    add solution for Jumping on the clouds

---
+++ Jumping_on_the_Clouds/src/Solution.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+
+class Solution {
+
+    static int jumpingOnClouds(int[] c) {
+        int n = c.length;
+        int jump = 0;
+        int i = 0;
+        while (i < n) {
+
+            if(i==n-1){
+                break;
+            }
+
+            if (c[i] == 0) {
+
+                if(c[i+1]==0){
+                    if(i+1==n-1){
+                        jump+=1;
+                        break;
+                    }
+                    if(c[i+2]==0){
+                        jump+=1;
+                        i+=2;
+                    }
+                    if(c[i+2]==1){
+                        jump+=1;
+                        i+=1;
+                    }
+                }
+                if(c[i+1]==1){
+                    if(i+1==n-1){
+                        break;
+                    }
+                    jump+=1;
+                    i+=2;
+                }
+
+
+            }
+        }
+        return jump;
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        int[] c= {0,0,0,1,0,0};
+        int result = jumpingOnClouds(c);
+
+    }
+}
