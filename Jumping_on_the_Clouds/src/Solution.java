17c11479d86ee078950c77d9e147d3899ec09c2c
Vvoox <khalil.daoulat@gmail.com>
2020-02-13 11:25:28 +0100

    add solution for Jumping on the clouds

--- Jumping_on_the_Clouds/src/Solution.java
+++ Jumping_on_the_Clouds/src/Solution.java
@@ -12,36 +12,35 @@ class Solution {
                 break;
             }
 
-            if (c[i] == 0) {
 
-                if(c[i+1]==0){
-                    if(i+1==n-1){
-                        jump+=1;
-                        break;
-                    }
-                    if(c[i+2]==0){
-                        jump+=1;
-                        i+=2;
-                    }
-                    if(c[i+2]==1){
-                        jump+=1;
-                        i+=1;
-                    }
+            if(c[i+1]==0){
+                if(i+1==n-1){
+                    jump+=1;
+                    break;
                 }
-                if(c[i+1]==1){
-                    if(i+1==n-1){
-                        break;
-                    }
+                if(c[i+2]==0 && (i+2)<n-1){
                     jump+=1;
                     i+=2;
                 }
-
-
+                if(c[i+2]==1){
+                    jump+=1;
+                    i+=1;
+                }
             }
+            if(c[i+1]==1){
+                if(i+1==n-1){
+                    break;
+                }
+                jump+=1;
+                i+=2;
+            }
+
+
         }
         return jump;
 
     }
+
 
 
     public static void main(String[] args) throws IOException {
