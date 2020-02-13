c3c136408f81a6c5d3fb436da65759648572f1e5
Vvoox <khalil.daoulat@gmail.com>
2020-02-13 11:36:32 +0100

    final solution for Jumping on the clouds

--- Jumping_on_the_Clouds/src/Solution.java
+++ Jumping_on_the_Clouds/src/Solution.java
@@ -11,18 +11,24 @@ class Solution {
             if(i==n-1){
                 break;
             }
-
-
             if(c[i+1]==0){
                 if(i+1==n-1){
                     jump+=1;
                     break;
                 }
-                if(c[i+2]==0 && (i+2)<n-1){
+                if(c[i+2]==0){
+                    if(i+2==n-1){
+                        jump+=1;
+                        break;
+                    }
                     jump+=1;
                     i+=2;
                 }
                 if(c[i+2]==1){
+                    if(i+2==n-1){
+                        jump+=1;
+                        break;
+                    }
                     jump+=1;
                     i+=1;
                 }
