2fcacbe4bee3f1f0c2d6130c146879257ee461c8
Vvoox <khalil.daoulat@gmail.com>
2020-02-14 22:49:27 +0100

    new solution for Repeated_String

--- Repeated_String/src/Repeated_String.java
+++ Repeated_String/src/Repeated_String.java
@@ -1,53 +1,51 @@
-import java.io.IOException;
+package com.company;
 
-public class Repeated_String {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Repeated_String implements Serializable{
 
     static long repeatedString(String s, long n) {
 
         int len = s.length();
         long res = 0;
-        int l = (int)n;
-        String[] tab = s.split("");
-        String[] result = new String[l];
-        int j=0;
-        for(int i = 0 ; i<l; i++){
-            if( calcul(tab,"a")==len ){
-                res = len*n;
-                break;
+
+        if(len==1 && s.equals("a")){
+            res = n;
+        }
+        else {
+            int l = (int)n;
+            String[] tab = s.split("");
+            String[] result = new String[l];
+            int j=0;
+            for (int i = 0; i < l; i++) {
+
+                result[i] = tab[j];
+                if(tab[j].equals("a")){
+                    res+=1;
+                }
+
+                j++;
+                if (j == len) {
+                    j = 0;
+                }
             }
-
-            result[i]=tab[j];
-
-            j++;
-            if(j==len) {
-                j=0;
-            }
-            if(i==n-1){
-                res = calcul(result,"a");
-
-            }
-
 
         }
+        System.out.println(res);
         return res;
 
     }
-    public static int calcul(String[] tab , String caracter ){
-        int n = tab.length;
-        int count = 0 ;
-        for(int i=0 ;i<n ; i++){
-            if(tab[i].equals(caracter)){
-                count+=1;
-            }
-        }
-        return count;
-    }
-
 
     public static void main(String[] args) throws IOException {
 
-        long n= 100000000;
-        String s = "a";
+        long n = 10;
+        String s = "abc";
 
 
         long result = repeatedString(s, n);
